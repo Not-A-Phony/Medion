e7fa74b66f54f7e7c27f7c1553cb23a47d5fc9a4
Amos-mathu <amos.mathu.0@gmail.com>
2026-06-23 10:52:49 +0300

    Step 2: Real Cloudinary Image Storage Integration

--- src/main/java/com/medion/hardwarestore/integration/storage/FileStorageService.java
+++ src/main/java/com/medion/hardwarestore/integration/storage/FileStorageService.java
@@ -1,26 +1,39 @@
 package com.medion.hardwarestore.integration.storage;
 
+import com.cloudinary.Cloudinary;
+import com.cloudinary.utils.ObjectUtils;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.util.UUID;
+import java.io.IOException;
+import java.util.Map;
 
 @Slf4j
 @Service
+@RequiredArgsConstructor
 public class FileStorageService {
+
+    private final Cloudinary cloudinary;
 
     public String uploadFile(MultipartFile file, String folderName) {
         log.info("Uploading file {} to Cloudinary folder {}", file.getOriginalFilename(), folderName);
-        // Stub: In reality, use Cloudinary SDK
-        // Map uploadResult = cloudinary.uploader().upload(file.getBytes(), ObjectUtils.asMap("folder", folderName));
-        // return uploadResult.get("secure_url").toString();
-        
-        return "https://res.cloudinary.com/demo/image/upload/v1/" + folderName + "/" + UUID.randomUUID() + ".jpg";
+        try {
+            Map uploadResult = cloudinary.uploader().upload(file.getBytes(), ObjectUtils.asMap("folder", folderName));
+            return uploadResult.get("secure_url").toString();
+        } catch (IOException e) {
+            log.error("Failed to upload image to Cloudinary", e);
+            throw new RuntimeException("Image upload failed");
+        }
     }
 
     public void deleteFile(String publicId) {
         log.info("Deleting file {} from Cloudinary", publicId);
-        // Stub: cloudinary.uploader().destroy(publicId, ObjectUtils.emptyMap());
+        try {
+            cloudinary.uploader().destroy(publicId, ObjectUtils.emptyMap());
+        } catch (IOException e) {
+            log.error("Failed to delete image from Cloudinary", e);
+        }
     }
 }
